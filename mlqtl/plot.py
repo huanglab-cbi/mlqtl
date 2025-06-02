7f6cfb04782f2290165b7c2dbf3a60a99f06f674
yang1ong <yanglong322@outlook.com>
2025-06-02 23:36:53 +0800

    add feature importance

--- mlqtl/plot.py
+++ mlqtl/plot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from pandas import DataFrame
 from typing import List, Tuple
 from .nda_typing import MatrixFloat64
 
@@ -46,3 +47,54 @@ def plot_graph(
     plt.show()
     if save:
         plt.savefig(f"{filename}.png", dpi=300, bbox_inches="tight")
+
+
+def plot_feature_importance(
+    feature_importance: DataFrame,
+    topn: int = 10,
+    save: bool = False,
+    filename: str = "feature_importance",
+) -> None:
+    """
+    Plot the feature importance of SNPs in different models
+    """
+    topn = 10
+    for row in feature_importance.iterrows():
+        model_name = row[0]
+        snps = row[1].index.tolist()
+        snp_number = {snp: str(i + 1) for i, snp in enumerate(snps)}
+        top_n = row[1].sort_values(ascending=False).head(topn)
+        y = top_n.values
+        x_labels_snp = top_n.index
+        x_labels_snp = [f"{snp} ({snp_number[snp]})" for snp in x_labels_snp]
+        x_positions = range(len(x_labels_snp))
+        plt.style.use("seaborn-v0_8-whitegrid")
+        plt.figure(figsize=(8, 6))
+        bar_width = 0.4
+        bars = plt.bar(
+            x_labels_snp,
+            y,
+            width=bar_width,
+            color="steelblue",
+            edgecolor="black",
+            alpha=1,
+        )
+        plt.xlabel(f"Top {topn} SNP", fontsize=14)
+        plt.xticks(x_positions, x_labels_snp, rotation=45, ha="right", fontsize=10)
+        plt.ylabel("Importance Score", fontsize=14)
+        plt.title(
+            f"SNP Feature Importance in Model: {model_name}",
+            fontsize=16,
+            fontweight="bold",
+            pad=20,
+        )
+
+        ax = plt.gca()
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        ax.yaxis.grid(True, linestyle="--", alpha=0.9)
+        ax.xaxis.grid(False)
+        plt.tight_layout()
+        plt.show()
+        if save:
+            plt.savefig(f"{filename}_{model_name}.png", dpi=300, bbox_inches="tight")
